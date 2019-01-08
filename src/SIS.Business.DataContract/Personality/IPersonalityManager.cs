757e08435acfa4497374462ac1159d5577140e67
zackigoldberg <zackigoldberg@gmail.com>
2019-01-08 11:42:40 -0500

    added personality manager

---
+++ src/SIS.Business.DataContract/Personality/IPersonalityManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HirePersonality.Business.DataContract.Personality
+{
+    public interface IPersonalityManager
+    {
+        Task<bool> CreatePersonality(PersonalityCreateDTO dto);
+    }
+}
---
+++ src/SIS.Business/Managers/Personality/PersonalityManager.cs
@@ -0,0 +1,16 @@
+﻿using HirePersonality.Business.DataContract.Personality;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HirePersonality.Business.Managers.Personality
+{
+    public class PersonalityManager : IPersonalityManager
+    {
+        public Task<bool> CreatePersonality(PersonalityCreateDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
